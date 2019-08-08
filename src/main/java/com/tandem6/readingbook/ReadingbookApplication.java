7cdaaf2bf929960b69f17667e5dc614e8302c204
TANDEM6 <nijin39@gmail.com>
2019-08-08 22:54:14 +0900

    20190808 Init Project

---
+++ src/main/java/com/tandem6/readingbook/ReadingbookApplication.java
@@ -0,0 +1,13 @@
+package com.tandem6.readingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tandem6/readingbook/ReadingbookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tandem6.readingbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReadingbookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
